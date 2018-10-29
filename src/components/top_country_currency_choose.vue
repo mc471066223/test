6ddde29e3b4b1bc9f3a6b5a234fa0cc7a1b3d8fa
Barry.Ma <47106623@qq.com>
2018-10-29 18:29:53 +0800

    change

--- src/components/top_country_currency_choose.vue
+++ src/components/top_country_currency_choose.vue
@@ -3,6 +3,7 @@
         <div class="top_right_country_more_arrow"></div>
         <div class="top_right_country_more_main">
             <h2 class="top_right_country_more_main_tit">Country/Region</h2>
+            
         </div>
     </div>
 </template>
