e485e32b1ee9537bc372189d05f75f4d6a69fad2
Barry.Ma <47106623@qq.com>
2018-10-29 19:02:13 +0800

    change

--- src/components/top_country_currency_choose.vue
+++ src/components/top_country_currency_choose.vue
@@ -3,13 +3,22 @@
         <div class="top_right_country_more_arrow"></div>
         <div class="top_right_country_more_main">
             <h2 class="top_right_country_more_main_tit">Country/Region</h2>
-            
+            <div class="top_right_country_choose">
+                <em :class="flag"></em>
+                <p class="top_right_country_choose_name">{{countryName}}</p>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name:'topCountryCurrencyChoose'
+    name:'topCountryCurrencyChoose',
+    data(){
+        return {
+            flag : 'cn',
+            countryName : 'China'
+        }
+    }
 }
 </script>
